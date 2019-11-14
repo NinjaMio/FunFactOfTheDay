d05a476c572bcb5681341633b981bec60ad21d8a
Congcong Zhang <cici.cc.zhang@gmail.com>
2019-11-14 17:48:10 +0000

    Added create, text validation, finished styling the page

--- app/controllers/funfacts_controller.rb
+++ app/controllers/funfacts_controller.rb
@@ -1,4 +1,24 @@
 class FunfactsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
+    @funfact = Funfact.where("created_at >= ?", Time.zone.now.beginning_of_day).last
+  end
+  
+  def new
+    @funfact = Funfact.new
+  end
+
+  def create
+    @funfact = Funfact.create(funfact_params)
+    if @funfact.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def funfact_params
+    params.require(:funfact).permit(:fact, :author)
   end
 end
--- app/models/funfact.rb
+++ app/models/funfact.rb
@@ -1,2 +1,4 @@
 class Funfact < ApplicationRecord
+  validates :fact, presence: true, length: { maximum: 140, minimum: 1 }
+  validates :author, presence: true, length: { maximum: 50, minimum: 1 }
 end
